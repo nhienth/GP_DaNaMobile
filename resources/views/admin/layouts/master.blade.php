320691e8e5c6f45b1202b231b4f9e571e7760aa3
HKTgaming <gokur304@gmail.com>
2022-09-25 15:57:02 +0700

    yeu Nhien

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- head -->
+    @include('admin.layouts.head')
+<!-- end head -->
+<body>
+    <!-- header -->
+    @include('admin.layouts.header')
+    <!-- end header -->
+
+    <!-- slidebar -->
+    <!-- end slidebar -->
+
+    <!-- footer -->
+    @include('admin.layouts.footer')
+    <!-- end footer -->
+</body>
+</html>
