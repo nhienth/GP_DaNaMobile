f22d163727810771ce0b942caac0728abe271f1b
unknown <anhcanh1007@gmail.com>
2022-09-26 12:38:22 +0700

    upload template client

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/admin', function () {
     return view('admin.index');
 });
+
+Route::get('/', function () {
+    return view('client.index');
+});
+
 // ->middleware(['auth'])->name('dashboard');
 
 // require __DIR__.'/auth.php';
-
