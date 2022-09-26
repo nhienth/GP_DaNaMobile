23c73acc6816ceddcaf8cd7ef680210c31516b08
nhienth <nhienth039@gmail.com>
2022-09-26 10:53:33 +0700

    fix template admin

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -36,26 +36,25 @@
     <!-- BEGIN: Footer-->
 
     @include('admin.layouts.footer')
-    <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
     <!-- END: Footer-->
 
 
     <!-- BEGIN: Vendor JS-->
-    <script src="app-assets/vendors/js/vendors.min.js')}}"></script>
+    <script src="{{asset('backend/app-assets/vendors/js/vendors.min.js')}}"></script>
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
-    <script src="app-assets/vendors/js/charts/apexcharts.min.js')}}"></script>
-    <script src="app-assets/vendors/js/extensions/toastr.min.js')}}"></script>
+    <script src="{{asset('backend/app-assets/vendors/js/charts/apexcharts.min.js')}}"></script>
+    <script src="{{asset('backend/app-assets/vendors/js/extensions/toastr.min.js')}}"></script>
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
-    <script src="app-assets/js/core/app-menu.js')}}"></script>
-    <script src="app-assets/js/core/app.js')}}"></script>
+    <script src="{{asset('backend/app-assets/js/core/app-menu.js')}}"></script>
+    <script src="{{asset('backend/app-assets/js/core/app.js')}}"></script>
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
-    <script src="app-assets/js/scripts/pages/dashboard-ecommerce.js')}}"></script>
+    <script src="{{asset('backend/app-assets/js/scripts/pages/dashboard-ecommerce.js')}}"></script>
     <!-- END: Page JS-->
 
     <script>
