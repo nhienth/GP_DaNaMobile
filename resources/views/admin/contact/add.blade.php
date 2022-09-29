0cfc13b38fd4a4b3ba03ae53650bac225a082681
Tien <tienlq2000@gmail.com>
2022-09-29 23:42:23 +0700

    Tien

---
+++ resources/views/admin/contact/add.blade.php
@@ -0,0 +1,124 @@
+@extends('admin.layouts.master')
+@section('main')
+<div class="app-content content ">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper container-xxl p-0">
+            <div class="content-header row">
+                <div class="content-header-left col-md-9 col-12 mb-2">
+                    <div class="row breadcrumbs-top">
+                        <div class="col-12">
+                            <h2 class="content-header-title float-start mb-0">Blog Edit</h2>
+                            <div class="breadcrumb-wrapper">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Home</a>
+                                    </li>
+                                    <li class="breadcrumb-item"><a href="#">Pages</a>
+                                    </li>
+                                    <li class="breadcrumb-item"><a href="#">Blog</a>
+                                    </li>
+                                    <li class="breadcrumb-item active">Edit
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-header-right text-md-end col-md-3 col-12 d-md-block d-none">
+                    <div class="mb-1 breadcrumb-right">
+                        <div class="dropdown">
+                            <button class="btn-icon btn btn-primary btn-round btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="grid"></i></button>
+                            <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item" href="app-todo.html"><i class="me-1" data-feather="check-square"></i><span class="align-middle">Todo</span></a><a class="dropdown-item" href="app-chat.html"><i class="me-1" data-feather="message-square"></i><span class="align-middle">Chat</span></a><a class="dropdown-item" href="app-email.html"><i class="me-1" data-feather="mail"></i><span class="align-middle">Email</span></a><a class="dropdown-item" href="app-calendar.html"><i class="me-1" data-feather="calendar"></i><span class="align-middle">Calendar</span></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-body">
+                <!-- Blog Edit -->
+                <div class="blog-edit-wrapper">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-start">
+                                        <div class="avatar me-75">
+                                            <img src="../../../app-assets/images/portrait/small/avatar-s-9.jpg" width="38" height="38" alt="Avatar" />
+                                        </div>
+                                        <div class="author-info">
+                                            <h6 class="mb-25">Chad Alexander</h6>
+                                            <p class="card-text">May 24, 2020</p>
+                                        </div>
+                                    </div>
+                                    <!-- Form -->
+                                    <form action="javascript:;" class="mt-2">
+                                        <div class="row">
+                                            <div class="col-md-6 col-12">
+                                                <div class="mb-2">
+                                                    <label class="form-label" for="blog-edit-slug">ID</label>
+                                                    <input type="text" id="blog-edit-slug" class="form-control" value="" />
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6 col-12">
+                                                <div class="mb-2">
+                                                    <label class="form-label" for="blog-edit-title">Name</label>
+                                                    <input type="text" id="blog-edit-title" class="form-control" value="" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="mb-2">
+                                                    <label class="form-label" for="blog-edit-category">Subject</label>
+                                                    
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6 col-12">
+                                                <div class="mb-2">
+                                                    <label class="form-label" for="blog-edit-slug">Email</label>
+                                                    <input type="text" id="blog-edit-slug" class="form-control" value="" />
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6 col-12">
+                                                <div class="mb-2">
+                                                    <label class="form-label" for="blog-edit-slug">Messager</label>
+                                                    <input type="text" id="blog-edit-slug" class="form-control" value="" />
+                                                </div>
+                                            </div>
+                                           
+                                            <div class="col-12 mb-2">
+                                                <div class="border rounded p-2">
+                                                    <h4 class="mb-1">Featured Image</h4>
+                                                    <div class="d-flex flex-column flex-md-row">
+                                                        <img src="../../../app-assets/images/slider/03.jpg" id="blog-feature-image" class="rounded me-2 mb-1 mb-md-0" width="170" height="110" alt="Blog Featured Image" />
+                                                        <div class="featured-info">
+                                                            <small class="text-muted">Required image resolution 800x400, image size 10mb.</small>
+                                                            <p class="my-50">
+                                                                <a href="#" id="blog-image-text">C:\fakepath\banner.jpg</a>
+                                                            </p>
+                                                            <div class="d-inline-block">
+                                                                <input class="form-control" type="file" id="blogCustomFile" accept="image/*" />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 mt-50">
+                                                <button type="submit" class="btn btn-primary me-1">Add</button>
+                                                <button type="reset" class="btn btn-outline-secondary">Cancel</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <!--/ Form -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ Blog Edit -->
+
+            </div>
+        </div>
+    </div>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,29 @@ Route::get('/admin/user', function () {
     return view('admin.user.list');
 });
 
+//voucher
+Route::get('/admin/voucher', function () {
+    return view('admin.voucher.list');
+});
+
+Route::get('/admin/voucher/add', function () {
+    return view('admin.voucher.add');
+});
+
+Route::get('/admin/voucher/edit', function () {
+    return view('admin.voucher.edit');
+});
+
+//variation
+Route::get('/admin/varitation', function () {
+    return view('admin.varitation.list');
+});
+
+//contact
+Route::get('/admin/contact', function () {
+    return view('admin.contact.list');
+});
+
 Route::get('/', function () {
     return view('client.index');
 });
