92a411e73af409c849922a8bd3bc43ec0d66d11a
unknown <anhcanh1007@gmail.com>
2022-10-04 00:05:46 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\SliderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,113 +44,112 @@ Route::prefix('/admin')->group(function () {
         return view('admin.index');
     });
     Route::prefix('/categories')->group(function () {
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.category.list');
         });
-        Route::get('/add', function() {
+        Route::get('/add', function () {
             return view('admin.category.create');
         });
-        Route::get('/edit', function() {
+        Route::get('/edit', function () {
             return view('admin.category.edit');
         });
     });
 
     Route::prefix('/subcate')->group(function () {
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.subcategory.list');
         });
-        Route::get('/add', function() {
+        Route::get('/add', function () {
             return view('admin.subcategory.create');
         });
-        Route::get('/edit', function() {
+        Route::get('/edit', function () {
             return view('admin.subcategory.edit');
         });
     });
 
     Route::prefix('/product')->group(function () {
-        Route::get('/add', function() {
+        Route::get('/add', function () {
             return view('admin.products.add');
         });
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.products.list');
         });
-        Route::get('/edit', function() {
+        Route::get('/edit', function () {
             return view('admin.products.edit');
         });
     });
 
     Route::prefix('/variation')->group(function () {
-        Route::get('/add', function() {
+        Route::get('/add', function () {
             return view('admin.variation.add');
         });
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.variation.list');
         });
-        Route::get('/edit', function() {
+        Route::get('/edit', function () {
             return view('admin.variation.edit');
         });
     });
 
     Route::prefix('/voucher')->group(function () {
-        Route::get('/add', function() {
+        Route::get('/add', function () {
             return view('admin.voucher.create');
         });
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.voucher.list');
         });
-        Route::get('/edit', function() {
+        Route::get('/edit', function () {
             return view('admin.voucher.edit');
         });
     });
 
     Route::prefix('/post')->group(function () {
-        Route::get('/add', function() {
+        Route::get('/add', function () {
             return view('admin.post.create');
         });
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.post.list');
         });
-        Route::get('/edit', function() {
+        Route::get('/edit', function () {
             return view('admin.post.edit');
         });
     });
 
     Route::prefix('/banner')->group(function () {
-        Route::get('/add', function() {
+        Route::get('/add', function () {
             return view('admin.banner.create');
         });
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.banner.list');
         });
-        Route::get('/edit', function() {
+        Route::get('/edit', function () {
             return view('admin.banner.edit');
         });
     });
 
     Route::prefix('/preview')->group(function () {
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.preview.list');
         });
     });
 
     Route::prefix('/contact')->group(function () {
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.contact.list');
         });
     });
 
     Route::prefix('/user')->group(function () {
-        Route::get('/list', function() {
+        Route::get('/list', function () {
             return view('admin.user.list');
         });
     });
-
 });
 
+Route::get('/testslider', [SliderController::class, 'getAll']);
 
 
 
 // ->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
-
+require __DIR__ . '/auth.php';
