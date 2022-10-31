4c65a2432a2c5f3756d9c3bd587c09945fd17e73
nhienth <nhienth039@gmail.com>
2022-10-31 09:16:04 +0700

    fix add product specfications

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -77,10 +77,11 @@
                                             <label class="form-label fs-6 fw-bolder" for="basic-addon-name">
                                                 {{$specfication->specification_name}}
                                             </label>
+                                           
                                             <input type="text" id="basic-addon-name" class="form-control" placeholder="Nhập thông số sản phẩm" name="{{$specfication->id}}_value" aria-label="Name" aria-describedby="basic-addon-name" required />
                                         </div>
                                     @endforeach
-
+                                    <input type="hidden" id="specification_cate" name="specification_cate" value="">
 
                                     <div class="mb-1">
                                         <label class="d-block form-label fs-5 fw-bolder" for="validationBioBootstrap">Mô tả</label>
@@ -113,6 +114,8 @@
     let specList = document.querySelectorAll('.show-spec');
 
     let valueOption = document.getElementById("select-country1").value;
+
+    document.getElementById('specification_cate').value = valueOption;
     
     specList.forEach((spec) => {
         let isSpecSeleted = spec.className.includes("show-" + valueOption); 
